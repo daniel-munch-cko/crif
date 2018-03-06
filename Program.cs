f83b10f80f8dd086a309937544233ae0ba6fc16e
Daniel Münch <daniel.munch@checkout.com>
2018-03-06 10:17:33 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+
+namespace CRIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<OrderCheckPortType> factory = null;  
+            OrderCheckPortType serviceProxy = null;  
+            Binding binding = null;
+
+            binding = new BasicHttpsBinding(BasicHttpsSecurityMode.Transport);  
+            factory = new ChannelFactory<OrderCheckPortType>(binding, new EndpointAddress("https://demo-ordercheck.deltavista.de/soap"));  
+            serviceProxy = factory.CreateChannel();
+
+            var input = new input(
+                new MessageContext()
+                {
+                   credentials = new Credentials()
+                   {
+                       user = "",
+                       password = ""
+                   } 
+                },
+                new OrderCheckRequest() 
+                {
+                    product = new Product() 
+                    {
+                        name = "CreditCheckConsumer",
+                        country = "DEU",
+                        proofOfInterest = "ABK"
+                    },
+                    searchedAddress = new SearchedAddress()
+                    {
+                        legalForm = LegalForm.PERSON,
+                        address = new Address() {
+                            name = "Falk",
+                            firstName = "Quintus",
+                            gender = Gender.MALE,
+                            dateOfBirth = 19680414,
+                            location = new Location()
+                            {
+                                street = "Rathausstrasse",
+                                house = "2",
+                                city = "Glücksburg",
+                                zip = "24960",
+                                country = "DEU"
+                            }
+                        },
+                        contact = new []{
+                            new Contact() 
+                            {
+                                item = "email",
+                                value = "f.quintus@crif.com"
+                            }
+                        }
+                    },
+                    clientData = new ClientData() 
+                    {
+                        reference = "Test_RCO_01",
+                        order = new Order() 
+                        {
+                            orderValue = 78.55F
+                        }
+                    }
+                }
+            );
+            
+            OrderCheck(serviceProxy, input).Wait();
+        }
+
+
+        public static async Task OrderCheck(OrderCheckPortType serviceProxy, input input) 
+        {
+            try
+            {
+                var result = await serviceProxy.orderCheckAsync(input);
+                Console.WriteLine($"Result: {result.orderCheckResponse.myDecision}");
+            }
+            catch(FaultException<www.deltavista.com.dspone.ordercheckif.V001.Error> ex) 
+            {
+                Console.WriteLine($"Error: {ex.Detail.messageText}");
+            }
+        }
+    }
+}
---
+++ WSDL/convert_wsdl.bat
@@ -0,0 +1,1 @@
+svcutil /t:code dspone-interface-ordercheck_V001.wsdl dspone-interface-ordercheck-soap_V001.xsd /o:CRIF.cs /l:cs
